55bd89b7262ce72997ac054497582bb05d57e453
Joaremio <joaremiorevoredo@gmail.com>
2026-08-01 20:17:56 -0300

    feat: adiciona configurações de autenticação

--- src/main/java/br/ufrn/imd/booking/repository/UserRepository.java
+++ src/main/java/br/ufrn/imd/booking/repository/UserRepository.java
@@ -3,8 +3,10 @@ package br.ufrn.imd.booking.repository;
 import br.ufrn.imd.booking.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID>
 {
+    Optional<User> findByEmail(String email);
 }
