22a7ea162df51e26071ebd0b6085255ec689796e
Joaremio <joaremiorevoredo@gmail.com>
2026-08-30 13:36:40 -0300

    feat: cria ResourceController com endpoints CRUD

---
+++ src/main/java/br/ufrn/imd/booking/controller/ResourceController.java
@@ -0,0 +1,54 @@
+package br.ufrn.imd.booking.controller;
+
+
+import br.ufrn.imd.booking.dto.ResourceRequestDTO;
+import br.ufrn.imd.booking.dto.ResourceResponseDTO;
+import br.ufrn.imd.booking.service.ResourceService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/resources")
+public class ResourceController {
+    private final ResourceService resourceService;
+
+    public ResourceController(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ResourceResponseDTO> createResource (@Valid @RequestBody ResourceRequestDTO data) {
+        ResourceResponseDTO resource = resourceService.createResource(data);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resource);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResourceResponseDTO> getResource(@PathVariable UUID id){
+        ResourceResponseDTO resource = resourceService.getResourceById(id);
+        return ResponseEntity.ok().body(resource);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResourceResponseDTO>> getAllResources() {
+        return ResponseEntity.ok(resourceService.getAllResources());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ResourceResponseDTO> updateResource(
+            @PathVariable UUID id,
+            @Valid @RequestBody ResourceRequestDTO data) {
+        return ResponseEntity.ok(resourceService.updateResource(id, data));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteResource(@PathVariable UUID id) {
+        resourceService.deleteResource(id);
+        return ResponseEntity.noContent().build();
+    }
+}
