9ac88ca202e827c9d230ecb924454a557234a4f2
Joaremio <joaremiorevoredo@gmail.com>
2026-08-30 19:59:15 -0300

    feat: cria classe BookingController para controle de requests na parte de reservas

---
+++ src/main/java/br/ufrn/imd/booking/controller/BookingController.java
@@ -0,0 +1,58 @@
+package br.ufrn.imd.booking.controller;
+
+
+import br.ufrn.imd.booking.dto.BookingRequestDTO;
+import br.ufrn.imd.booking.dto.BookingResponseDTO;
+import br.ufrn.imd.booking.entity.User;
+import br.ufrn.imd.booking.service.BookingService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<BookingResponseDTO> createBooking(
+            @Valid @RequestBody BookingRequestDTO data,
+            @AuthenticationPrincipal User user
+    ){
+        BookingResponseDTO booking = bookingService.createBooking(data, user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(booking);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<List<BookingResponseDTO>> getMyBookings(@AuthenticationPrincipal User user){
+        return ResponseEntity.ok().body(bookingService.getBookingsByUser(user.getId()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookingResponseDTO> getBookingById(
+            @PathVariable UUID id,
+            @AuthenticationPrincipal User user
+    ) {
+        return ResponseEntity.ok(bookingService.getBookingById(id, user));
+    }
+
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<Void> cancelBooking(
+            @PathVariable UUID id,
+            @AuthenticationPrincipal User user
+    ) {
+        bookingService.cancelBooking(id, user);
+        return ResponseEntity.noContent().build();
+    }
+}
